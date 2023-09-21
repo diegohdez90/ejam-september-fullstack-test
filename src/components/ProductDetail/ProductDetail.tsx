6e24dee3e3ca733190664b51372b430df0175a89
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-21 17:58:52 -0600

    feat: init product card

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -1,5 +1,39 @@
-const ProductDetail = () => {
-  return <div>Product detail</div>
+import LeftSide from "./LeftSide";
+import RightSide from "./RightSide";
+
+interface Props {
+  productTitle: object;
+  productName: string;
+  smallName: string;
+  featuredComment: object;
+  score: number;
+  price: number;
+  discountedPrice: number;
+  applyDiscountPrice: boolean;
+  amountInStock: number;
+  description: string
+  thumbnailImage: string;
+  features: any[];
+  promotion: object;
+  isGuaranteed: boolean
+}
+
+
+const ProductDetail = ({
+  productTitle,
+  promotion,
+  smallName,  
+  discountedPrice
+}: Props) => {
+  return <div className="w-full grid grid-cols-2 p-4 gap-8">
+    <LeftSide/>
+    <RightSide
+      productTitle={productTitle}
+      promotion={promotion}
+      smallName={smallName}
+      discountedPrice={discountedPrice}
+    />
+  </div>
 }
 
 export default ProductDetail;
