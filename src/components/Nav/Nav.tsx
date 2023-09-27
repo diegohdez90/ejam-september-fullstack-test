52871906905b29f07e2fe0ae982064cb9a907fa2
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-09-27 14:23:23 -0600

    feat: add nav with logos

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,35 @@
+import Image from "next/image";
+
 const Nav = () => {
-  return <div>
-    Nav
+  return <div className="flex flex-row w-full justify-between">
+    <div>
+      <Image
+        src="/clarifon.svg"
+        alt="calrifon"
+        width={0}
+        height={0}
+        fill={false}
+        style={{height: "100%", width:"100%", position: 'initial'}}
+      />
+    </div>
+    <div className="flex flex-row gap-8">
+      <Image
+        src="/mcafee.svg"
+        alt="mcafee"
+        width={0}
+        height={0}
+        fill={false}
+        style={{height: "100%", width:"100%", position: 'initial'}}
+      />
+      <Image
+        src="/norton.svg"
+        alt="norton"
+        width={0}
+        height={0}
+        fill={false}
+        style={{height: "100%", width:"100%", position: 'initial'}}
+      />
+    </div>
   </div>
 }
 
